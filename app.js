81437378923c269a35d364e9b1598a2c3e0cdbd6
tmax818 <tylermaxwell661@gmail.com>
2020-03-09 08:53:43 -0700

    simplify

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-console.log('hi from app.js')
-
 const title = React.createElement(
     'h1',
-    { id: 'main-title', title: 'This is a title' },
+    { id: 'title'},
     "The most basic React element"
 );
 
-const desc = React.createElement(
-    'p',
-    null,
-    'This is just the CDN and React.createElement()'
-);
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
-);
-
 ReactDOM.render(
-    header,
+    title,
     document.getElementById('root')
 );
